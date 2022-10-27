6ecd92c1c227abe581dd069376b3ca4511cbdcc2
IT20615734 <it20615734@my.sliit.lk>
2022-10-28 01:34:09 +0530

    payment activity  step 01 done

--- app/src/main/java/com/example/my_grocery_app/MainActivity.java
+++ app/src/main/java/com/example/my_grocery_app/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.my_grocery_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_item)
+    RecyclerView recycleritem;
+    @BindView(R.id.mainLayout)
+    RecyclerView mainLayout;
+
+    @BindView(R.id.badge)
+    NotificationBadge badge;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        dinuka payment
-//        test 01
+
+
     }
 }
