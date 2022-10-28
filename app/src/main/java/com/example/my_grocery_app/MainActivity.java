b860aaacf0c3eceb08872c31acb40f8a2399dead
IT20615734 <it20615734@my.sliit.lk>
2022-10-29 00:43:25 +0530

    cart activity  step 02 done

--- app/src/main/java/com/example/my_grocery_app/MainActivity.java
+++ app/src/main/java/com/example/my_grocery_app/MainActivity.java
@@ -1,21 +1,47 @@
 package com.example.my_grocery_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Notification;
+import android.content.ClipData;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.my_grocery_app.Listener.ICartLoadListener;
+import com.example.my_grocery_app.Listener.IItemLoadListener;
+import com.example.my_grocery_app.Model.CartModel;
+import com.example.my_grocery_app.Model.ItemModel;
+import com.example.my_grocery_app.utils.SpaceItemDecoration;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.FirebaseDatabase;
+import com.nex3z.notificationbadge.NotificationBadge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements IItemLoadListener, ICartLoadListener {
 
     @BindView(R.id.recycler_item)
-    RecyclerView recycleritem;
+    RecyclerView recyclerItem;
+
     @BindView(R.id.mainLayout)
-    RecyclerView mainLayout;
+    RelativeLayout mainLayout;
 
     @BindView(R.id.badge)
     NotificationBadge badge;
 
+    @BindView(R.id.btncart)
+    FrameLayout btncart;
+
+    IItemLoadListener itemLoadListener;
+    ICartLoadListener cartLoadListener;
 
 
     @Override
@@ -23,6 +49,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+        loadItemFromFirebase();
+    }
+
+    private void loadItemFromFirebase() {
+        List<ItemModel> itemModels = new ArrayList<>();
+        FirebaseDatabase.getInstance()
+                .getReference(Item)
+    }
+
+    private void init() {
+        ButterKnife.bind(this);
+
+        itemLoadListener = this;
+        cartLoadListener = this;
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
+        recyclerItem.setLayoutManager(gridLayoutManager);
+        recyclerItem.addItemDecoration(new SpaceItemDecoration());
+
+    }
+
+    @Override
+    public void onItemLoadSuccess(List<ItemModel> itemModelList) {
+
+    }
+
+    @Override
+    public void onItemLoadFailed(String message) {
+
+    }
+
+    @Override
+    public void onCartLoadSuccess(List<CartModel> cartModelList) {
+
+    }
+
+    @Override
+    public void onCartLoadFailed(String message) {
 
     }
 }
+
+
+
+
+
