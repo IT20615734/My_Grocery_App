cb5659c79a3148fa57a03a83cb59c776c91f746c
IT20615734 <it20615734@my.sliit.lk>
2022-10-29 11:03:42 +0530

    cart activity  step 04 done

--- app/src/main/java/com/example/my_grocery_app/Adapter/MyItemAdapter.java
+++ app/src/main/java/com/example/my_grocery_app/Adapter/MyItemAdapter.java
@@ -1,14 +1,77 @@
 package com.example.my_grocery_app.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.my_grocery_app.Model.ItemModel;
+import com.example.my_grocery_app.R;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class MyItemAdapter extends RecyclerView.Adapter<MyItemAdapter.MyItemViewHolder> {
+
+
+    private Context context;
+    private List<ItemModel> itemModelList;
+
+    public MyItemAdapter(Context context, List<ItemModel> itemModelList) {
+        this.context = context;
+        this.itemModelList = itemModelList;
+    }
+
+    @NonNull
+    @Override
+    public MyItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return MyItemViewHolder(LayoutInflater.from(context).inflate(R.layout.layout_item,parent,false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyItemViewHolder holder, int position) {
+
+        Glide.with(context)
+                .load(itemModelList.get(position).getImage())
+                .into(holder.imageView);
+        holder.txtprice.setText(new StringBuilder($).append(itemModelList.get(position).getPrice()));
+        holder.txtName.setText(new StringBuilder().append(itemModelList.get(position).getName()));
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemModelList.size();
+    }
+
     public class MyItemViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.imageView)
+        ImageView imageView;
+        @BindView(R.id.txtName)
+        TextView txtName;
+        @BindView(R.id.textprice)
+        TextView txtprice;
+
+        private Unbinder unbinder;
+
+
+
+
         public MyItemViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            unbinder = ButterKnife.bind(this,itemView);
+
         }
     }
 }
